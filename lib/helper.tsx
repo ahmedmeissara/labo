f33c6bf4b6400b333d5a496c3ac2109c3b07a05d
ahmedmeissara <66229501+ahmedmeissara@users.noreply.github.com>
2023-04-30 20:32:19 +0100

    Update helper.tsx

--- lib/helper.tsx
+++ lib/helper.tsx
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/";
+const BASE_URL = "https://labo-hs5apkmtz-ahmedmeissara-gmailcom.vercel.app/";
 
 // Get all specimens
 export const getSpecimens = async () => {
