df9dc6b5678251f6849eb8e92f39ff5aa49fd784
ahmedmeissara <66229501+ahmedmeissara@users.noreply.github.com>
2023-04-30 20:19:22 +0100

    new

--- next.config.js
+++ next.config.js
@@ -4,18 +4,5 @@ const nextConfig = {
     // Use the `raw-loader` for importing `.txt` files during tests
     test: /.txt$/,
     use: 'raw-loader',
-  },
-  typescript: {
-    // Dangerously allow production builds to successfully complete even if your project has type errors
-    ignoreBuildErrors: true,
-  },
-};
-
-module.exports = {
-  // Ignore ESLint errors during builds
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // Merge the `nextConfig` object into the exports
-  ...nextConfig,
-};
+  }}
+  module.exports = nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 "use client"; // this is a client component 👈🏽
+
 import React from 'react';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import './globals.css';
@@ -8,11 +9,6 @@ import { Provider } from 'react-redux';
 // Initialize the query client
 const queryClient = new QueryClient();
 
-// Define metadata
-export const metadata = {
-  title: 'CRUD APP',
-  description: 'hello this ahmed',
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
